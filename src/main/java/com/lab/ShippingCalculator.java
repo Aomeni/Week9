bd162f7118bf89809b6d19f1fd43e55b9b56dae0
Kiên <139709646+Aomeni@users.noreply.github.com>
2026-05-02 12:59:01 +0700

    up

--- src/main/java/com/lab/ShippingCalculator.java
+++ src/main/java/com/lab/ShippingCalculator.java
@@ -1,7 +1,20 @@
 package com.lab;
 
 public class ShippingCalculator {
-    public double calculateShipping(double weight, double distance) {
-        return weight * 0.5 + distance * 0.1;
+
+    public double calculate(double weight, String type) {
+        if (weight <= 0) {
+            throw new IllegalArgumentException("Weight must be positive");
+        }
+
+        if ("EXPRESS".equals(type)) {
+            return weight * 5000 + 20000;
+        }
+
+        if ("STANDARD".equals(type)) {
+            return weight * 3000;
+        }
+
+        throw new IllegalArgumentException("Unknown type: " + type);
     }
 }
