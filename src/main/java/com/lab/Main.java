6de9a37d0d4387fde5c84995569493185e0d172b
Kiên <139709646+Aomeni@users.noreply.github.com>
2026-05-02 11:56:04 +0700

    Fix 4: update JUnit 5 build and CI workflow

--- src/main/java/com/lab/Main.java
+++
@@ -1,9 +0,0 @@
-package com.lab;
-
-public class Main {
-    public static void main(String[] args) {
-        ShippingCalculator calc = new ShippingCalculator();
-        double cost = calc.calculateShipping(10.0, 100.0);
-        System.out.println("Shipping cost: " + cost);
-    }
-}
